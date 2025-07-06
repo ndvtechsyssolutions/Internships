d48670bbec4c12ef358bed3c5cc7bb5d95d742fe
ribkaaramalla322 <rebeccaaramalla@gmail.com>
2025-07-06 16:22:19 +0530

    NDV_Code_By_RibkaA_DynamicProductListing_react.js

---
+++ NDV_Code_By_DynamicProductListing/DynamicProductListing_react.js
@@ -0,0 +1,17 @@
+//App.css
+  .App {
+  text-align: center;
+  padding: 2rem;
+}
+
+input, select {
+  margin: 1rem;
+  padding: 0.5rem;
+}
+
+.product-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
+}
