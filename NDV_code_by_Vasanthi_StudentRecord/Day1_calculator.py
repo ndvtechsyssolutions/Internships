1039aba9c673a34efd474a0867ae19b8c90d94ee
mummanavasanthi <153973630+mummanavasanthi@users.noreply.github.com>
2025-07-11 23:31:42 +0530

    Add files via upload

---
+++ NDV_code_by_Vasanthi_StudentRecord/Day1_calculator.py
@@ -0,0 +1,13 @@
+#Calculator
+num1=int(input())
+num2=int(input())
+sum=num1+num2
+print(f"Sum of {num1} and {num2} is {sum}")
+diff=num1-num2
+print(f"Difference of {num1} and {num2} is {diff}")
+mul=num1*num2
+print(f"Multiplication of {num1} and {num2} is {mul}")
+div=num1/num2
+print(f"Division of {num1} and {num2} is {div}")
+mod=num1%num2
+print(f"Modulus of {num1} and {num2} is {mod}")
